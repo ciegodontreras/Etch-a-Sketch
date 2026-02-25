f183f08904c5ca47481183b30c37872d1bed0568
Diego Contreras <diegocontreras401@gmail.com>
2026-02-25 12:03:04 -0600

    created input value for creating a personalized grid, still needs work

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('.container'); 
+const createGrid = document.querySelector('.createGrid'); 
+const columnNum = document.getElementById('columns'); 
+const rowNum = document.getElementById('rows'); 
 
 const grid = (columns, rows) => {
     // container.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     // container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     let totalBoxes = columns * rows; 
     for (let i = 1; i <= totalBoxes; i++) {
-        let newBox = document.createElement('div'); 
+        let newBox = document.createElement('div');
         newBox.className = `box${i}`;
         newBox.className = 'boxes'  
         newBox.style.textAlign = 'center'; 
@@ -16,5 +19,8 @@ const grid = (columns, rows) => {
     };
 }
 
-  
-grid(2,2); 
+createGrid.addEventListener('click', () => {
+    let columnValue = columnNum.value; 
+    let rowValue = rowNum.value;
+    grid(columnValue, rowValue); 
+}); 
